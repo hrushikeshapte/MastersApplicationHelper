97b89f72804f2be9a957b85cffdc6eec00bfbaa6
aptehrushikesh <apte.hrushikesh@gmail.com>
2015-07-04 15:57:27 -0700

    added mysql insert to store info entered

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,6 +3,7 @@ var app = angular.module('MSApp', []);
 app.controller("mainController", function ($scope, $http) {
 
     $scope.info = {};
+    $scope.question = 1 ;
     var i = 1;
 
     $scope.init = function(){
@@ -14,54 +15,63 @@ app.controller("mainController", function ($scope, $http) {
         $scope.info[i++] = $scope.email;
         $scope.one = false;
         $scope.two = true;
+        $scope.question = 2 ;
     };
 
     $scope.quantInformation = function(){
         $scope.two = false;
         $scope.three = true;
         $scope.info[i++] = $scope.quant;
+        $scope.question = 3 ;
     }
 
     $scope.verbalInformation = function(){
         $scope.three = false;
         $scope.four = true;
         $scope.info[i++] = $scope.verbal;
+        $scope.question = 4 ;
     }
 
     $scope.totalInformation = function(){
         $scope.four = false;
         $scope.five = true;
         $scope.info[i++] = $scope.total;
+        $scope.question = 5 ;
     }
 
     $scope.toeflInformation = function(){
         $scope.five = false;
         $scope.six = true;
         $scope.info[i++] = $scope.toefl;
+        $scope.question = 6 ;
     }
 
     $scope.marksInformation = function(){
         $scope.six = false;
         $scope.seven  = true;
         $scope.info[i++] = $scope.marks;
+        $scope.question = 7 ;
     }
 
     $scope.workYearsInformation = function(){
         $scope.seven = false;
         $scope.eight = true;
         $scope.info[i++] = $scope.years;
+        $scope.question = 8 ;
     }
 
     $scope.relevanceInformation = function(){
         $scope.eight = false;
         $scope.nine = true;
         $scope.info[i++] = $scope.relevance;
+        $scope.question = 9 ;
     }
 
     $scope.companyInformation = function(){
         $scope.nine = false;
         $scope.ten = true;
         $scope.info[i++] = $scope.company;
+        $scope.question = 10 ;
     }
 
     $scope.paperInformation = function(){
@@ -69,42 +79,21 @@ app.controller("mainController", function ($scope, $http) {
         $scope.eleven = true;
         $scope.eleven = true;
         $scope.info[i++] = $scope.paper;
+        $scope.question = 11 ;
     }
 
     $scope.voluntaryInformation = function(){
-        console.log($scope.info);
         $scope.eleven = false;
         $scope.twelve = true;
         $scope.info[i++] = $scope.voluntary;
+
+        $http.post('/info',$scope.info)
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(data) {
+                alert("Cannot get response from server");
+            });
     }
-
-
-
-
-    /*$scope.formInfo = {};
-
-     $http.get('/api/information')
-     .success(function(data){
-     console.log(data);
-     $scope.information = data;
-     })
-
-     .error(function(data){
-     alert ("Cannot fetch request");
-     })
-
-     $scope.createInformation = function(){
-     console.log("here");
-     /*     $http.post('/api/information',$scope.formInfo)
-     .success(function(data){
-     $scope.formInfo = {};
-     $scope.information = data;
-     console.log(data);
-     })
-
-     .error(function(data){
-     alert("error creating request");
-     }) */
-    ///};
 
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var dbHelper = require('./dbHelper.js');
 var app = express();
+var bodyParser = require('body-parser');
 
 var connection = dbHelper.initializeConnection({
     host: "localhost",
@@ -11,5 +12,29 @@ var connection = dbHelper.initializeConnection({
 
 
 app.use(express.static( __dirname+"/public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.post('/info', function(req, res) {
+    console.log(req.body);
+    res.send("ok");
+
+   var queryString = 'INSERT INTO userinfo VALUES("'+req.body[1]+'",'+req.body[2]+','+req.body[3]+','+req.body[4]+','+req.body[5]+','+req.body[6]+','+req.body[7]+',"'+req.body[8]+'","'+req.body[9]+'",'+req.body[10]+',"'+req.body[11]+'")';
+    connection.query(queryString, function(err, rows, fields) {
+        if (err)
+        {
+            console.log("error");
+        }
+
+        else{
+           console.log("success");
+        }
+    });
+
+
+});
 
 app.listen(2000);
