1402293a9e8391d7b6608d5f78c5f997ec7d6f56
aptehrushikesh <apte.hrushikesh@gmail.com>
2015-07-04 01:19:50 -0700

    second commit

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -2,9 +2,85 @@ var app = angular.module('MSApp', []);
 
 app.controller("mainController", function ($scope, $http) {
 
-    console.log("here");
+    $scope.info = {};
+    var i = 1;
 
-    $scope.test = "sucker";
+    $scope.init = function(){
+        $scope.one = true;
+    }
+
+
+    $scope.emailInformation = function(){
+        $scope.info[i++] = $scope.email;
+        $scope.one = false;
+        $scope.two = true;
+    };
+
+    $scope.quantInformation = function(){
+        $scope.two = false;
+        $scope.three = true;
+        $scope.info[i++] = $scope.quant;
+    }
+
+    $scope.verbalInformation = function(){
+        $scope.three = false;
+        $scope.four = true;
+        $scope.info[i++] = $scope.verbal;
+    }
+
+    $scope.totalInformation = function(){
+        $scope.four = false;
+        $scope.five = true;
+        $scope.info[i++] = $scope.total;
+    }
+
+    $scope.toeflInformation = function(){
+        $scope.five = false;
+        $scope.six = true;
+        $scope.info[i++] = $scope.toefl;
+    }
+
+    $scope.marksInformation = function(){
+        $scope.six = false;
+        $scope.seven  = true;
+        $scope.info[i++] = $scope.marks;
+    }
+
+    $scope.workYearsInformation = function(){
+        $scope.seven = false;
+        $scope.eight = true;
+        $scope.info[i++] = $scope.years;
+    }
+
+    $scope.relevanceInformation = function(){
+        $scope.eight = false;
+        $scope.nine = true;
+        $scope.info[i++] = $scope.relevance;
+    }
+
+    $scope.companyInformation = function(){
+        $scope.nine = false;
+        $scope.ten = true;
+        $scope.info[i++] = $scope.company;
+    }
+
+    $scope.paperInformation = function(){
+        $scope.ten = false;
+        $scope.eleven = true;
+        $scope.eleven = true;
+        $scope.info[i++] = $scope.paper;
+    }
+
+    $scope.voluntaryInformation = function(){
+        console.log($scope.info);
+        $scope.eleven = false;
+        $scope.twelve = true;
+        $scope.info[i++] = $scope.voluntary;
+    }
+
+
+
+
     /*$scope.formInfo = {};
 
      $http.get('/api/information')
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var dbHelper = require('./dbHelper.js');
 var app = express();
+
+var connection = dbHelper.initializeConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database : "applicationdb"
+});
+
 
 app.use(express.static( __dirname+"/public"));
 
